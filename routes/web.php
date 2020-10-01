aefcbb2a8c51fa42931fd3e1a19ee91b4b0dae03
HarueSan <oonnaka2012@gmail.com>
2020-10-02 00:40:31 +0700

    finish mint
    add customer_information หน้าข้อมูลลูกค้า
    finish customer
    1. register หน้าสมัครสมาชิก
    2. detail product หน้าข้อมูลสินค้า
    sellman
    1. detail product หน้าข้อมูลสินค้า

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,15 @@ Route::get('/', function () {
 Route::get('/cuslogin', function () {
     return view('customer/login_user');
 });
+Route::get('/detailpd', function () {
+    return view('customer/detail_product');
+});
+Route::get('/detailpd_sellman', function () {
+    return view('sellman/detailproduct_sellman');
+});
+Route::get('/register', function () {
+    return view('customer/register_customer');
+});
+Route::get('/cusinformation', function () {
+    return view('customer/customer_information');
+});
