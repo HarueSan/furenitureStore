52602af35c1a8a080bcb27974205873b8912aeb5
HarueSan <oonnaka2012@gmail.com>
2020-10-12 01:43:10 +0700

    แก้หน้าใหม่
    แก้หน้า login ของ model
    แก้หน้าจองสินค้า ของ บุ๋น
    ใช้ bootstarp ช่วย

--- resources/views/sellman/order/orders.blade.php
+++ resources/views/sellman/order/orders.blade.php
@@ -1,42 +1,22 @@
 
 <html>
-    <h1 style="text-align: center; font-size: 150%; margin-top: 30px">
-    คำสั่งซื้อสินค้า
-    </h1>
-    <br>
     <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <style>
-            table, td, th {
-            border: 1px solid black;
-            text-align: center;
-            }
+        <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-            table {
-            border-collapse: collapse;
-            width: 50%;
-            }
-
-            table.center {
-                margin-left:auto; 
-                margin-right:auto;
-            }
-
-
-            th {
-            height: 35px;
-            background-color: black;
-            color: white;
-            }
-
-            tr {
-                
-            }
-
-        </style>
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
     </head>
-    <body >
-        <table class = center>
+    <body style="margin-top:1em;">
+    <div class="container">
+    <div class="text-center">
+            <h2>คำสั่งซื้อสินค้า</h2>
+        </div>
+        <table class="table">
             <tr>
                 <th> ID ลูกค้า</th>
                 <th>วันที่สั่งซื้อ</th>
@@ -80,6 +60,9 @@
                 </td>
             </tr>
         </table>
+    </div>
+
+      
     </body>
 
 </html>
